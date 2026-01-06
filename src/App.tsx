dc8420a662c06c5f427b0a25628a9d490ca1ed2d
xialeistudio <xialeistudio@gmail.com>
2026-01-06 20:46:44 +0800

    feature: integrate shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <main>
-      <h1>Welcome to Tauri + React</h1>
-    </main>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,11 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
